4fe0519e1edb23e3b079b6b9c0868be635415fbe
kangkyeongseo <kks_big@naver.com>
2023-05-19 12:03:48 +0900

    Password  Change Page

--- pages/setting/index.tsx
+++ pages/setting/index.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import { useState } from "react";
 
-export default function Members() {
+export default function Setting() {
   const router = useRouter();
   return (
     <>
@@ -33,9 +33,11 @@ export default function Members() {
         </div>
         <ul className="flex flex-col justify-center items-center  gap-2 mt-8 text-gray-700">
           <li className="cursor-pointer hover:text-gray-900">
-            <Link href={"/name-change"}>닉네임 변경</Link>
+            <Link href={"/setting/name-change"}>닉네임 변경</Link>
           </li>
-          <li className="cursor-pointer hover:text-gray-900">비밀번호 변경</li>
+          <li className="cursor-pointer hover:text-gray-900">
+            <Link href={"/setting/password-change"}>비밀번호 변경</Link>
+          </li>
           <li className="cursor-pointer hover:text-gray-900">로그아웃</li>
         </ul>
       </div>
