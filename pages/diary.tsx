9574142f9d353df82adb963db7853368ba3ea862
kangkyeongseo <kks_big@naver.com>
2023-05-23 16:36:51 +0900

    Add Card Component with Kind

--- pages/diary.tsx
+++ pages/diary.tsx
@@ -1,3 +1,4 @@
+import AddCard from "@/components/AddCard";
 import Card from "@/components/Card";
 import Link from "next/link";
 import { useRouter } from "next/router";
@@ -358,172 +359,20 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div
-        className={[
-          "fixed top-0 w-full h-full bg-[rgba(0,0,0,0.8)] z-10",
-          addCard ? "" : "hidden",
-        ].join(" ")}
-      >
-        <div className="max-w-2xl bg-slate-600 mt-32 mx-auto p-8 rounded-xl shadow-2xl">
-          <h3 className="text-xl font-bold text-white text-center">
-            새로운 다이어리
-          </h3>
-          <div className="grid grid-cols-2 items-center mt-8">
-            <div className="ml-6">
-              <div
-                className={[
-                  "flex flex-col w-56 h-80 rounded-xl shadow-2xl p-4 relative",
-                  `bg-${bgColor}-500`,
-                ].join(" ")}
-              >
-                <div className="flex justify-center items-center ">
-                  <span className="text-sm text-white">{date}</span>
-                </div>
-                <div className="text-center text-lg font-bold text-white mt-4 break-words">
-                  {title.length === 0 ? "무슨 일이 있었나요?" : title}
-                </div>
-                <div className="text-white mt-4">
-                  <div className="break-words">{todo}</div>
-                </div>
-              </div>
-            </div>
-            <div>
-              <form className="flex flex-col gap-2">
-                <div className="flex flex-col gap-2">
-                  <label className="text-white">제목</label>
-                  <input
-                    type="text"
-                    className="px-2 py-1 border-none rounded-xl focus:outline-none"
-                    value={title}
-                    onChange={onTitleChange}
-                  />
-                </div>
-                <div className="flex flex-col gap-2">
-                  <label className="text-white">내용</label>
-                  <input
-                    type="text"
-                    className="px-2 py-1 border-none rounded-xl focus:outline-none"
-                    value={todo}
-                    onChange={onToDoChange}
-                  />
-                </div>
-                <div className="flex flex-col gap-2">
-                  <label className="text-white">날짜</label>
-                  <input
-                    type="date"
-                    className="px-2 py-1 border-none rounded-xl focus:outline-none"
-                    onChange={onDateChange}
-                  />
-                </div>
-                <div className="flex flex-col gap-2">
-                  <label className="text-white">배경색</label>
-                  <div className="flex gap-4">
-                    <div>
-                      <label htmlFor="blue">
-                        <div
-                          className={[
-                            "w-12 h-12 rounded-lg bg-blue-500",
-                            bgColor === "blue" ? "border-2 border-white" : "",
-                          ].join(" ")}
-                        ></div>
-                      </label>
-                      <input
-                        type="radio"
-                        id="blue"
-                        name="bgColor"
-                        className="hidden"
-                        onClick={() => setBgColor("blue")}
-                      />
-                    </div>
-                    <div>
-                      <label htmlFor="green">
-                        <div
-                          className={[
-                            "w-12 h-12 rounded-lg bg-green-500",
-                            bgColor === "green" ? "border-2 border-white" : "",
-                          ].join(" ")}
-                        ></div>
-                      </label>
-                      <input
-                        type="radio"
-                        id="green"
-                        name="bgColor"
-                        className="hidden"
-                        onClick={() => setBgColor("green")}
-                      />
-                    </div>
-                    <div>
-                      <label htmlFor="yellow">
-                        <div
-                          className={[
-                            "w-12 h-12 rounded-lg bg-yellow-500",
-                            bgColor === "yellow" ? "border-2 border-white" : "",
-                          ].join(" ")}
-                        ></div>
-                      </label>
-                      <input
-                        type="radio"
-                        id="yellow"
-                        name="bgColor"
-                        className="hidden"
-                        onClick={() => setBgColor("yellow")}
-                      />
-                    </div>
-                    <div>
-                      <label htmlFor="slate">
-                        <div
-                          className={[
-                            "w-12 h-12 rounded-lg bg-slate-500",
-                            bgColor === "slate" ? "border-2 border-white" : "",
-                          ].join(" ")}
-                        ></div>
-                      </label>
-                      <input
-                        type="radio"
-                        id="slate"
-                        name="bgColor"
-                        className="hidden"
-                        onClick={() => setBgColor("slate")}
-                      />
-                    </div>
-                    <div>
-                      <label htmlFor="red">
-                        <div
-                          className={[
-                            "w-12 h-12 rounded-lg bg-red-500",
-                            bgColor === "red" ? "border-2 border-white" : "",
-                          ].join(" ")}
-                        ></div>
-                      </label>
-                      <input
-                        type="radio"
-                        id="red"
-                        name="bgColor"
-                        className="hidden"
-                        onClick={() => setBgColor("red")}
-                      />
-                    </div>
-                  </div>
-                </div>
-              </form>
-            </div>
-          </div>
-          <div className="flex justify-between mt-8">
-            <button
-              className="w-72 p-2 rounded-xl bg-white hover:bg-gray-200"
-              onClick={onAddCard}
-            >
-              취소하기
-            </button>
-            <button
-              className="w-72 p-2 rounded-xl bg-blue-500 text-white hover:bg-blue-600"
-              onClick={onAddCard}
-            >
-              생성하기
-            </button>
-          </div>
-        </div>
-      </div>
+      {addCard ? (
+        <AddCard
+          title={title}
+          contents={todo}
+          date={date}
+          bgColor={bgColor}
+          onTitleChange={onTitleChange}
+          onToDoChange={onToDoChange}
+          onDateChange={onDateChange}
+          setBgColor={setBgColor}
+          onAddCard={onAddCard}
+          kind="diary"
+        />
+      ) : null}
       <div
         className={[
           "fixed top-0 w-full h-full bg-[rgba(0,0,0,0.8)] z-10",
