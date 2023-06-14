e2564727531ba5476727e5cf4985584309e9771b
kangkyeongseo <kks_big@naver.com>
2023-06-14 20:47:55 +0900

    Logout

---
+++ pages/api/users/me/logout.ts
@@ -0,0 +1,20 @@
+import withHandler from "@/libs/sever/withHandler";
+import { withIronSessionApiRoute } from "iron-session/next";
+import { NextApiRequest, NextApiResponse } from "next";
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<{ ok: boolean }>
+) {
+  req.session.destroy();
+  return res.status(200).json({ ok: true });
+}
+
+export default withIronSessionApiRoute(
+  withHandler({ method: "POST", handler }),
+  {
+    cookieName: "carddiarysession",
+    password:
+      "kjspioejh290ejvnw0498th1-di9ivnb21-390th2-9ei9vn2-39rthb21-390gnv-0102jrt94-9ghn2-=e90vn1-230",
+  }
+);
--- pages/setting/index.tsx
+++ pages/setting/index.tsx
@@ -1,7 +1,20 @@
 import Layout from "@/components/Layout";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function Setting() {
+  const router = useRouter();
+  const onLogout = async () => {
+    await fetch("/api/users/me/logout", {
+      method: "POST",
+    })
+      .then((response) => response.json())
+      .then((json) => {
+        if (json.ok) {
+          router.push("/enter");
+        }
+      });
+  };
   return (
     <Layout title="설정" canGoBack={true}>
       <ul className="flex flex-col justify-center items-center  gap-2 mt-8 text-gray-700">
@@ -11,7 +24,9 @@ export default function Setting() {
         <li className="cursor-pointer hover:text-gray-900">
           <Link href={"/setting/password-change"}>비밀번호 변경</Link>
         </li>
-        <li className="cursor-pointer hover:text-gray-900">로그아웃</li>
+        <li className="cursor-pointer hover:text-gray-900" onClick={onLogout}>
+          로그아웃
+        </li>
       </ul>
     </Layout>
   );
