d54ed4ac6552ca002626a8fa2fe60b193ecefcda
kangkyeongseo <kks_big@naver.com>
2023-07-10 17:34:49 +0900

    Diary Detail Page

--- pages/diary/[id]/index.tsx
+++ pages/diary/[id]/index.tsx
@@ -1,28 +1,28 @@
 import useDate from "@/libs/client/useDate";
-import { Todo } from "@prisma/client";
+import { Diary } from "@prisma/client";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
-export default function TodoDetail() {
+export default function DiaryDetail() {
   const router = useRouter();
-  const [todo, setTodo] = useState<Todo>();
+  const [diary, setDiary] = useState<Diary>();
   const onPeriodChange = (date: any) => {
     const today = new Date();
     const periodDate = Math.round((+date - +today) / 1000 / 3600 / 24);
     return periodDate;
   };
-  const getTodo = async () => {
-    const data = await (await fetch(`/api/todo/${router.query.id}`)).json();
-    setTodo(data.todo);
+  const getDiary = async () => {
+    const data = await (await fetch(`/api/diary/${router.query.id}`)).json();
+    setDiary(data.diary);
   };
   useEffect(() => {
     if (router.query.id) {
-      getTodo();
+      getDiary();
     }
   }, [router]);
   return (
     <div className="w-full h-screen flex justify-center items-center bg-[rgba(0,0,0,0.8)]">
-      {todo && (
+      {diary && (
         <div className="max-w-2xl px-8 pt-4 pb-8 flex flex-col gap-4 text-white bg-slate-600 rounded-xl shadow-2xl ">
           <div
             className="self-end cursor-pointer text-3xl"
@@ -33,28 +33,16 @@ export default function TodoDetail() {
           <div
             className={[
               "w-[600px] p-8 flex flex-col gap-8 rounded-xl",
-              `bg-${todo.bgColor}-500`,
+              `bg-${diary.bgColor}-500`,
             ].join(" ")}
           >
             <header className="flex justify-between">
-              <div>{useDate(new Date(todo.date))}</div>
-              <div className="flex ">
-                {[1, 2, 3, 4, 5].map((star) => (
-                  <div
-                    key={star}
-                    className={
-                      todo.importance >= star ? "text-yellow-300" : "text-white"
-                    }
-                  >
-                    ★
-                  </div>
-                ))}
-              </div>
+              <div>{useDate(new Date(diary.date))}</div>
             </header>
-            <h1 className="text-center text-2xl font-bold">{todo.title}</h1>
-            <p className="min-h-[300px]">{todo.content}</p>
+            <h1 className="text-center text-2xl font-bold">{diary.title}</h1>
+            <p className="min-h-[300px]">{diary.content}</p>
             <div className="self-end">
-              {onPeriodChange(new Date(todo.date))}일 남음
+              {onPeriodChange(new Date(diary.date))}일 남음
             </div>
           </div>
           <div className="flex justify-between">
@@ -63,7 +51,7 @@ export default function TodoDetail() {
             </button>
             <button
               className="w-48 p-2 rounded-xl bg-slate-500 hover:bg-slate-700"
-              onClick={() => router.push(`/todo/${todo.id}/edit-card`)}
+              onClick={() => router.push(`/diary/${diary.id}/edit-card`)}
             >
               수정하기
             </button>
