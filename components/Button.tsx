a79d00679a2a2c29d342294307c470c70921fa4e
kangkyeongseo <kks_big@naver.com>
2023-08-24 16:50:27 +0900

    fix: tailwind css 우선순위로 인한 style 미적용 버그 수정

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,16 +1,15 @@
 interface ButtonProp {
   text: string;
-  color: string;
+  bgColor: string;
   onClick?: () => void;
 }
 
-export default function Button({ text, color, onClick }: ButtonProp) {
+export default function Button({ text, bgColor, onClick }: ButtonProp) {
   return (
     <button
-      className={[
-        "w-full text-white rounded-2xl shadow-md py-3",
-        `bg-${color}-500 hover:bg-${color}-600`,
-      ].join(" ")}
+      className={["w-full text-white rounded-2xl shadow-md py-3", bgColor].join(
+        " "
+      )}
       onClick={onClick}
     >
       {text}
