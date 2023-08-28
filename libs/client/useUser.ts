1fb8830141873423d02586fc754f98c8e225101f
kangkyeongseo <kks_big@naver.com>
2023-08-28 14:32:36 +0900

    refactor: useUser 데이터 처리 swr으로 변경

--- libs/client/useUser.ts
+++ libs/client/useUser.ts
@@ -1,15 +1,10 @@
-import { User } from "@prisma/client";
-import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import useSWR from "swr";
 
 export default function useUser() {
   const router = useRouter();
-  const [data, setData] = useState<{ ok: boolean; user: User }>();
-  useEffect(() => {
-    fetch("/api/users/me")
-      .then((response) => response.json())
-      .then((json) => setData(json));
-  }, []);
+  const { data } = useSWR("/api/users/me");
   useEffect(() => {
     if (data && !data.ok) {
       router.push("/enter");
