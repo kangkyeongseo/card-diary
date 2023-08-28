ad460e5d425742a83846fbf991dac5daf7de937b
kangkyeongseo <kks_big@naver.com>
2023-08-28 14:33:40 +0900

    style: 조건문 중괄호 추가

--- pages/api/users/me/index.ts
+++ pages/api/users/me/index.ts
@@ -1,17 +1,20 @@
-import withHandler from "@/libs/sever/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
-import client from "@/libs/sever/client";
-import { User } from "@prisma/client";
+import withHandler, { ResponseType } from "@/libs/sever/withHandler";
 import withSession from "@/libs/sever/withSession";
+import client from "@/libs/sever/client";
 
 async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<{ ok: boolean; user?: User }>
+  res: NextApiResponse<ResponseType>
 ) {
   const { session } = req;
-  if (!session.user) return res.status(403).json({ ok: false });
+  if (!session.user) {
+    return res.status(403).json({ ok: false });
+  }
   const user = await client.user.findUnique({ where: { id: session.user.id } });
-  if (!user) return res.status(403).json({ ok: false });
+  if (!user) {
+    return res.status(403).json({ ok: false });
+  }
   return res.status(200).json({ ok: true, user });
 }
 
