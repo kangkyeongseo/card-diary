ea05ca435134023e318f2d02f5fb6e02398a1410
kangkyeongseo <kks_big@naver.com>
2023-09-18 17:22:33 +0900

    feat: todo 데이터 DELETE 기능 추가

--- pages/todo/[id]/index.tsx
+++ pages/todo/[id]/index.tsx
@@ -1,28 +1,30 @@
-import useDate from "@/libs/client/useDate";
-import { Todo } from "@prisma/client";
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
+import useSWR from "swr";
+import useDate from "@/libs/client/useDate";
 
 export default function TodoDetail() {
   const router = useRouter();
-  const [todo, setTodo] = useState<Todo>();
+  const { data } = useSWR(`/api/todo/${router.query.id}`);
+
   const onPeriodChange = (date: any) => {
     const today = new Date();
     const periodDate = Math.round((+date - +today) / 1000 / 3600 / 24);
     return periodDate;
   };
-  const getTodo = async () => {
-    const data = await (await fetch(`/api/todo/${router.query.id}`)).json();
-    setTodo(data.todo);
+
+  const onDelete = () => {
+    fetch(`/api/todo/${router.query.id}`, { method: "DELETE" })
+      .then((response) => response.json())
+      .then((json) => {
+        if (json.ok) {
+          router.push("/");
+        }
+      });
   };
-  useEffect(() => {
-    if (router.query.id) {
-      getTodo();
-    }
-  }, [router]);
+
   return (
     <div className="w-full h-screen flex justify-center items-center bg-[rgba(0,0,0,0.8)]">
-      {todo && (
+      {data && (
         <div className="max-w-2xl px-8 pt-4 pb-8 flex flex-col gap-4 text-white bg-slate-600 rounded-xl shadow-2xl ">
           <div
             className="self-end cursor-pointer text-3xl"
@@ -33,17 +35,19 @@ export default function TodoDetail() {
           <div
             className={[
               "w-[600px] p-8 flex flex-col gap-8 rounded-xl",
-              `bg-${todo.bgColor}-500`,
+              `bg-${data.todo.bgColor}-500`,
             ].join(" ")}
           >
             <header className="flex justify-between">
-              <div>{useDate(new Date(todo.date))}</div>
+              <div>{useDate(new Date(data.todo.date))}</div>
               <div className="flex ">
                 {[1, 2, 3, 4, 5].map((star) => (
                   <div
                     key={star}
                     className={
-                      todo.importance >= star ? "text-yellow-300" : "text-white"
+                      data.todo.importance >= star
+                        ? "text-yellow-300"
+                        : "text-white"
                     }
                   >
                     ★
@@ -51,19 +55,24 @@ export default function TodoDetail() {
                 ))}
               </div>
             </header>
-            <h1 className="text-center text-2xl font-bold">{todo.title}</h1>
-            <p className="min-h-[300px]">{todo.content}</p>
+            <h1 className="text-center text-2xl font-bold">
+              {data.todo.title}
+            </h1>
+            <p className="min-h-[300px]">{data.todo.content}</p>
             <div className="self-end">
-              {onPeriodChange(new Date(todo.date))}일 남음
+              {onPeriodChange(new Date(data.todo.date))}일 남음
             </div>
           </div>
           <div className="flex justify-between">
-            <button className="w-48 p-2 rounded-xl bg-red-500 hover:bg-red-600">
+            <button
+              className="w-48 p-2 rounded-xl bg-red-500 hover:bg-red-600"
+              onClick={onDelete}
+            >
               삭제하기
             </button>
             <button
               className="w-48 p-2 rounded-xl bg-slate-500 hover:bg-slate-700"
-              onClick={() => router.push(`/todo/${todo.id}/edit-card`)}
+              onClick={() => router.push(`/todo/${data.todo.id}/edit-card`)}
             >
               수정하기
             </button>
