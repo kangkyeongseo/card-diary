f0c23383a5f8ba8fc57cda19eb3627fff168cfe5
kangkyeongseo <kks_big@naver.com>
2023-10-09 12:50:00 +0900

    refactor: CardTable 컴포넌트 적용으로 코드 개선

--- pages/diary/index.tsx
+++ pages/diary/index.tsx
@@ -5,8 +5,9 @@ import { Diary } from "@prisma/client";
 import useUser from "@/libs/client/useUser";
 import Card from "@/components/Card";
 import SideBar from "@/components/SideBar/SideBar";
+import CardTable from "@/components/CardTable/CardTable";
 
-interface IDiaryResponse {
+interface DiaryResponse {
   ok: boolean;
   diarys: Diary[];
 }
@@ -14,32 +15,33 @@ interface IDiaryResponse {
 export default function Home() {
   const router = useRouter();
   const { user } = useUser();
-  const { data } = useSWR<IDiaryResponse>(
+  const { data } = useSWR<DiaryResponse>(
     user
       ? router.query.list
         ? `/api/diary?list=${router.query.list}`
         : "/api/diary"
       : null
   );
-  const [diarys, setDiarys] = useState<Diary[]>();
+  const [diarys, setDiarys] = useState<DiaryResponse>();
 
   useEffect(() => {
     if (!data) {
       return;
     }
     if (!router.query.month) {
-      setDiarys(data.diarys);
+      setDiarys(data);
     } else {
-      setDiarys(
-        data.diarys.filter((diary) => {
+      setDiarys({
+        ok: data.ok,
+        diarys: data.diarys.filter((diary) => {
           if (
             new Date(diary.date).getMonth() ===
             Number(router.query.month) - 1
           ) {
             return diary;
           }
-        })
-      );
+        }),
+      });
     }
   }, [data, router.query.month]);
 
@@ -47,77 +49,7 @@ export default function Home() {
     <div>
       <div className="grid grid-cols-[300px_1fr] ">
         <SideBar user={user} listType="diary" />
-        <div className="flex flex-col p-12">
-          <ul className="grid grid-cols-3 items-center w-full max-w-xl bg-slate-700 rounded-[20px] mx-auto mb-12 shadow-2xl text-white">
-            <li
-              className={
-                router.pathname === "/"
-                  ? "w-full bg-slate-500 text-center p-2 rounded-[20px] border border-white hover:text-slate-900"
-                  : "text-center hover:text-slate-900"
-              }
-              onClick={() => router.push("/")}
-            >
-              계획
-            </li>
-            <li
-              className={
-                router.pathname === "/diary"
-                  ? "w-full bg-slate-500 text-center p-2 rounded-[20px] border border-white hover:text-slate-900"
-                  : "text-center hover:text-slate-900"
-              }
-              onClick={() => router.push("/diary")}
-            >
-              다이어리
-            </li>
-            <li
-              className={
-                router.pathname === "/memo"
-                  ? "w-full bg-slate-500 text-center p-2 rounded-[20px] border border-white hover:text-slate-900"
-                  : "text-center hover:text-slate-900"
-              }
-              onClick={() => router.push("/memo")}
-            >
-              메모
-            </li>
-          </ul>
-          <div className="grid grid-cols-[repeat(auto-fit,minmax(14rem,max-content))] justify-center items-center gap-8 ">
-            {diarys &&
-              diarys.map((diary) => (
-                <Card
-                  key={diary.id}
-                  id={diary.id}
-                  title={diary.title}
-                  contents={diary.content}
-                  date={diary.date}
-                  bgColor={diary.bgColor}
-                  kind="diary"
-                />
-              ))}
-            <div
-              className="flex justify-center items-center w-full max-w-[14rem] h-80 rounded-xl border border-dashed text-white hover:scale-105"
-              onClick={() =>
-                router.push(`/diary/add-card?list=${router.query.list}`)
-              }
-            >
-              <div>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="w-12 h-12"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M12 4.5v15m7.5-7.5h-15"
-                  />
-                </svg>
-              </div>
-            </div>
-          </div>
-        </div>
+        <CardTable kind="diary" data={diarys} />
       </div>
     </div>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,31 @@
 import React from "react";
+import { useRouter } from "next/router";
+import useSWR from "swr";
 import useUser from "@/libs/client/useUser";
 import SideBar from "@/components/SideBar/SideBar";
 import CardTable from "@/components/CardTable/CardTable";
+import { Todo } from "@prisma/client";
+
+interface TodoResponse {
+  ok: boolean;
+  todos: Todo[];
+}
 
 export default function Home() {
+  const router = useRouter();
   const { user } = useUser();
+  const { data } = useSWR<TodoResponse>(
+    user
+      ? router.query.list
+        ? `/api/todo?list=${router.query.list}`
+        : "/api/todo"
+      : null
+  );
 
   return (
     <div className="grid grid-cols-[300px_1fr] ">
       <SideBar user={user} listType="todo" />
-      <CardTable />
+      <CardTable kind="todo" data={data} />
     </div>
   );
 }
